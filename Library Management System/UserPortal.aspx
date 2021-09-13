e1d448461e1ff3cb63c0e427337050e003554a3a
Anis Khoja <52759828+aniskhoja@users.noreply.github.com>
2021-09-13 00:07:18 -0400

    Add missing files to the LMS core

---
+++ Library Management System/UserPortal.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UserPortal.aspx.cs" Inherits="Library_Management_System.UserProtal" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style>
+        #UserRentedList tr th:first-child,#UserRentedList tr td:first-child,#UserRentedList tr th:nth-child(2),#UserRentedList tr td:nth-child(2){
+            display:none;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Button ID="Logout" OnClick="Logout_Click" runat="server" Text="LogOut" />
+        <p>
+            <asp:Label ID="Label1" runat="server" Text="List of Book" Font-Bold="True"></asp:Label>
+        </p>
+        <asp:GridView ID="UserPortalGrid" runat="server" AllowSorting="True" AutoGenerateColumns="false" AllowPaging="true" OnSorting="UserPortalGrid_Sorting"
+            PageSize="4" OnPageIndexChanging="UserPortalGrid_PageIndexChanging" OnSelectedIndexChanging="UserPortalGrid_SelectedIndexChanging">
+            <Columns>
+                <asp:BoundField ReadOnly="true" DataField="Id" HeaderText="Id" />
+                <asp:BoundField ReadOnly="true" DataField="Title" HeaderText="Book Title" SortExpression="title" />
+                <asp:BoundField ReadOnly="true" DataField="Available" HeaderText="Availablity" SortExpression="available" />
+                <asp:BoundField DataField="Quantity" HeaderText="Quanitity" SortExpression="quantity" />
+
+                <asp:TemplateField HeaderText="Auther">
+                    <ItemTemplate>
+
+                        <asp:ListBox ID="AuthorList" runat="server" DataSource='<%#Library_Management_System.SQLOperations.BookData.GetAuthorsbyBookId((int)Eval("Id"))%>' DataTextField="Name"></asp:ListBox>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+                <asp:CommandField SelectText="Rent a Book" ShowSelectButton="true" />
+
+
+            </Columns>
+
+        </asp:GridView>
+
+        <asp:Label ID="Error_msg" runat="server" ForeColor="#FF3300" Text="Book is not Available for rent" Visible="False"></asp:Label>
+
+
+        <p>
+            <asp:Label ID="Label2" runat="server" Text="List of Rented Book" Font-Bold="True"></asp:Label>
+        </p>
+        <asp:GridView ID="UserRentedList" runat="server" AutoGenerateColumns="false"
+            OnSelectedIndexChanging="UserRentedList_SelectedIndexChanging">
+            <Columns>
+                <asp:BoundField ReadOnly="true" DataField="Id" HeaderText="Id" />
+                <asp:BoundField ReadOnly="true" DataField="BookId" HeaderText="Book Id" />
+                <asp:BoundField ReadOnly="true" DataField="Title" HeaderText="Book Title" />
+                <asp:BoundField DataField="BorrowDate" HeaderText="Borrow Date and Time" />
+
+
+
+                <asp:CommandField SelectText="Retrun this Book" ShowSelectButton="true" />
+
+
+            </Columns>
+
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Library Management System/UserPortal.aspx.cs
@@ -0,0 +1,147 @@
+﻿using Library_Management_System.Model;
+using Library_Management_System.SQLOperations;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Library_Management_System
+{
+    public partial class UserProtal : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (Session["UserId"] == null)
+            {
+                Response.Redirect("HomePage.aspx");
+            }
+
+            if (!IsPostBack)
+            {
+                FillUserManager();
+                FillUserRentedBook();
+            }
+        }
+        private void FillUserManager()
+        {
+
+            DataTable dtrslt = BookData.GetAll();
+
+            UserPortalGrid.DataSource = dtrslt;
+            UserPortalGrid.DataBind();
+            ViewState["dirState"] = dtrslt;
+            ViewState["sortdr"] = "Asc";
+
+        }
+
+
+
+
+
+
+
+        protected void UserPortalGrid_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            DataTable dtrslt = (DataTable)ViewState["dirState"];
+            UserPortalGrid.PageIndex = e.NewPageIndex;
+            if (ViewState["SortColumn"] != null)
+            {
+                dtrslt.DefaultView.Sort = ViewState["SortColumn"] + " " + ViewState["sortdr"];
+            }
+            UserPortalGrid.DataSource = dtrslt;
+            UserPortalGrid.DataBind();
+        }
+
+
+
+        protected void Logout_Click(object sender, EventArgs e)
+        {
+            ViewState["UserId"] = "";
+            Response.Redirect("HomePage.aspx");
+        }
+
+        protected void UserPortalGrid_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            DataTable dtrslt = (DataTable)ViewState["dirState"];
+            if (dtrslt.Rows.Count > 0)
+            {
+                if (Convert.ToString(ViewState["sortdr"]) == "Asc")
+                {
+                    dtrslt.DefaultView.Sort = e.SortExpression + " Desc";
+                    ViewState["SortColumn"] = e.SortExpression;
+                    ViewState["sortdr"] = "Desc";
+                }
+                else
+                {
+                    dtrslt.DefaultView.Sort = e.SortExpression + " Asc";
+                    ViewState["SortColumn"] = e.SortExpression;
+                    ViewState["sortdr"] = "Asc";
+                }
+                UserPortalGrid.DataSource = dtrslt;
+                ViewState["dirState"] = dtrslt;
+                UserPortalGrid.DataBind();
+
+
+            }
+        }
+
+        protected void UserPortalGrid_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+            GridViewRow row = (GridViewRow)UserPortalGrid.Rows[e.NewSelectedIndex];
+            int bookid = Convert.ToInt32(UserPortalGrid.Rows[e.NewSelectedIndex].Cells[0].Text);
+
+            Book book = BookData.GetbyId(bookid);
+
+
+            if (book.Available == 0)
+            {
+                Error_msg.Visible = true;
+            }
+            else
+            {
+                Error_msg.Visible = false;
+                BookData.Update(bookid, book.Quantity, book.Available - 1);
+                UsersData.RentaBook(Convert.ToInt32(UserPortalGrid.Rows[e.NewSelectedIndex].Cells[0].Text), Convert.ToInt32(Session["UserId"]));
+
+            }
+            // UserPortalGrid.EditIndex = -1;
+            FillUserManager();
+            FillUserRentedBook();
+        }
+
+        protected void UserRentedList_SelectedIndexChanging(object sender, GridViewSelectEventArgs e)
+        {
+            Error_msg.Visible = false;
+            GridViewRow row = (GridViewRow)UserRentedList.Rows[e.NewSelectedIndex];
+            int bookid = Convert.ToInt32(UserRentedList.Rows[e.NewSelectedIndex].Cells[1].Text);
+
+            Book book = BookData.GetbyId(bookid);
+
+            UsersData.RetrunBook(Convert.ToInt32(UserRentedList.Rows[e.NewSelectedIndex].Cells[0].Text));
+
+            BookData.Update(bookid, book.Quantity, book.Available + 1);
+
+            FillUserRentedBook();
+            FillUserManager();
+       
+
+
+        }
+
+        private void FillUserRentedBook()
+        {
+
+            DataTable UserRentedBookdtrslt = UsersData.GetRentBookById(Convert.ToInt32(Session["UserId"]));
+
+            UserRentedList.DataSource = UserRentedBookdtrslt;
+            UserRentedList.DataBind();
+            ViewState["UserRentedBookdtrslt"] = UserRentedBookdtrslt;
+
+
+        }
+    }
+}
